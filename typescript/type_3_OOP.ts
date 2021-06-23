3ac66606dd1b87d0dcfc3def3e9cade0f6ab9006
hty019 <hty019@gmail.com>
2021-06-23 23:57:38 +0900

    익혀가는중

--- typescript/type_3_OOP.ts
+++ typescript/type_3_OOP.ts
@@ -1,14 +1,15 @@
 class Employee {
-     
+
     constructor(
-        private _fullName:string, 
-        private _age:number, 
-        private _jobTitle:string, 
-        private _hourlyRate:number, 
+        private _fullName:string,
+        private _age:number,
+        private _jobTitle:string,
+        private _hourlyRate:number,
         public workingHoursPerWeek:number){
-        
+
     }
     printEmployeeDetails = ():void => {
+        console.log('tempString');
         console.log(`${this.fullName}의 직업은 ${this.jobTitle} 이고 일주일의 수입은 ${this.hourlyRate*this.workingHoursPerWeek} 달러이다.`)
     }
 }
